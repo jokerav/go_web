814dbd3c17e36c9407ec93dc304e1e0ddcf3c42e
jokerav <jokeravdeveloper@gmail.com>
2023-02-07 22:41:05 +0200

    form deploy

--- src/Components/contact/contact.js
+++ src/Components/contact/contact.js
@@ -7,18 +7,16 @@ const Contact = () => {
     return (
         <section className='contact'>
             <div className='contact-pic'></div>
-            <div className='contact-inner'>
+
             <Container nameClass='contact-container'>
-                <form className='contact-form'>
+                <form className='contact-form' name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+                    <input type="hidden" name="form-name" value="contact" />
                     <p className='form-title'>Request Callback</p>
                     <input name='name' className='form-input input-name' defaultValue='Enter your name'/>
                     <input name='email' className='form-input input-email' type='email' defaultValue='Enter email*'/>
                     <Button type='submit' nameClass='contactsBtn'>Send</Button>
                 </form>
             </Container>
-            </div>
-
-
 
 </section>
 )
