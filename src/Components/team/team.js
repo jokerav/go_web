d0033714349df81932d2428738297efd898719a1
jokerav <jokeravdeveloper@gmail.com>
2023-02-11 16:22:49 +0200

    filter картинки

--- src/Components/team/team.js
+++ src/Components/team/team.js
@@ -16,27 +16,29 @@ import person3wp from '../../images/images/team/person3.webp'
 import person3jpeg2x from '../../images/images/team/person3@2x.jpg'
 import person3jpeg from '../../images/images/team/person3.jpg'
 
-
-const Team = ()=>{
-    return(
+const Team = () => {
+    return (
         <section className='team'>
             <Container>
                 <p className='team-above-title'>Who we are</p>
                 <p className='team-title'>Our Professional Team</p>
-                <p className='team-sub-title'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, sapiente!</p>
+                <p className='team-sub-title'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto,
+                    sapiente!</p>
                 <ul className='team-list'>
                     <li className='team-item'>
-                        <picture>
-                            <source  srcSet={`${person1wp2x} 2x, ${person1jpeg2x} 2x, ${person1wp} 1x, ${person1jpeg} 1x`}
-                            />
-                            <img src="#" alt='фото сотрудника' className='team-photo'/>
-                        </picture>
+                            <picture>
+                                <source
+                                    srcSet={`${person1wp2x} 2x, ${person1jpeg2x} 2x, ${person1wp} 1x, ${person1jpeg} 1x`}
+                                />
+                                <img src="#" alt='фото сотрудника' className='team-photo'/>
+                            </picture>
                         <p className='team-name'>John Doe</p>
                         <p className='team-position'>President</p>
                     </li>
                     <li className='team-item'>
                         <picture>
-                            <source  srcSet={`${person2wp2x} 2x, ${person2jpeg2x} 2x, ${person2wp} 1x, ${person2jpeg} 1x`}
+                            <source
+                                srcSet={`${person2wp2x} 2x, ${person2jpeg2x} 2x, ${person2wp} 1x, ${person2jpeg} 1x`}
                             />
                             <img src="#" alt='фото сотрудника' className='team-photo'/>
                         </picture>
@@ -45,7 +47,8 @@ const Team = ()=>{
                     </li>
                     <li className='team-item'>
                         <picture>
-                            <source  srcSet={`${person3wp2x} 2x, ${person3jpeg2x} 2x, ${person3wp} 1x, ${person3jpeg} 1x`}
+                            <source
+                                srcSet={`${person3wp2x} 2x, ${person3jpeg2x} 2x, ${person3wp} 1x, ${person3jpeg} 1x`}
                             />
                             <img src="#" alt='фото сотрудника' className='team-photo'/>
                         </picture>
